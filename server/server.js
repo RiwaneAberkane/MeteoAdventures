b7bcfcd41870f735ddf2723f3a0ff9b21afa1a32
RiwaneAberkane <riwane.aberkane.pro@outlook.fr>
2023-10-30 22:27:01 +0100

    :tada: begin

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000; // Port sur lequel le serveur écoutera
+const PORT = process.env.PORT || 3000; // Port sur lequel le serveur écoutera
 
-// Middleware, routes, et autres configurations peuvent être ajoutés ici
+// Middleware, routes, et autres
 
 app.get("/", (req, res) => {
   res.send("Hello, World!"); // Exemple de route de base
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT} 🚀🚀🚀`);
 });
