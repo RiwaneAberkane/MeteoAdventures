cfdc279c27567233ec77f8294f27ff9a99897777
RiwaneAberkane <riwane.aberkane.pro@outlook.fr>
2023-11-01 17:15:46 +0100

    modification weatherService.js

--- server/services/weatherService.js
+++ server/services/weatherService.js
@@ -1,9 +1,9 @@
-// Utilisation d'Axios pour faire un appel API
 const axios = require("axios");
+const config = require("./config"); // Chemin du fichier de configuration
 
 const fetchWeatherData = async (location) => {
-  const API_KEY = "YOUR_API_KEY";
-  const API_URL = `https://api.example.com/weather?location=${location}&apiKey=${API_KEY}`; //$$$$$$$$$$$
+  const API_KEY = config.API_KEY; // Utilisation de la clé d'API depuis le fichier de configuration
+  const API_URL = `https://api.example.com/weather?location=${location}&apiKey=${API_KEY}`;
 
   try {
     const response = await axios.get(API_URL);
