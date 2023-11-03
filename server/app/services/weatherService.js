e941d7a6c9ec7d7076d15d420b679d64bed06d5e
RiwaneAberkane <riwane.aberkane.pro@outlook.fr>
2023-11-03 22:23:19 +0100

    work in progress

--- server/app/services/weatherService.js
+++ server/app/services/weatherService.js
@@ -1,6 +1,6 @@
 const fetchWeatherData = async (location) => {
   try {
-    const API_KEY = "1a2947f5e4670c0d7151aa0f488df436";
+    const API_KEY = process.env.API_KEY;
     const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
     const response = await fetch(API_URL);
 
