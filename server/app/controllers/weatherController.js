d49e3b515f75ac37614d11fe67a2f8a6c8d65fcf
RiwaneAberkane <riwane.aberkane.pro@outlook.fr>
2023-11-03 15:02:13 +0100

    recover object

--- server/app/controllers/weatherController.js
+++ server/app/controllers/weatherController.js
@@ -2,9 +2,9 @@ const { fetchWeatherData } = require("../services/weatherService");
 
 const getWeather = async (req, res) => {
   try {
-    console.log(req.query.location);
     const data = await fetchWeatherData(req.query.location);
     res.json(data);
+    console.log(data);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -13,5 +13,3 @@ const getWeather = async (req, res) => {
 module.exports = {
   getWeather,
 };
-
-
--- server/app/services/weatherService.js
+++ server/app/services/weatherService.js
@@ -1,14 +1,20 @@
 const fetchWeatherData = async (location) => {
   try {
-    const API_KEY = process.env.API_KEY; // Utilisation de la clé d'API depuis le fichier de configuration
+    const API_KEY = "1a2947f5e4670c0d7151aa0f488df436";
     const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
     const response = await fetch(API_URL);
-    console.log(response);
-    return response;
+
+    if (response.ok) {
+      const data = await response.json(); // Convertit la réponse en format JSON
+      return data; // Renvoie les données JSON reçues de l'API
+    } else {
+      throw new Error("Erreur lors de la récupération des données météo");
+    }
   } catch (error) {
-    return "Error fetching";
+    throw new Error("Erreur lors de la récupération des données météo");
   }
 };
+
 module.exports = {
   fetchWeatherData,
 };
