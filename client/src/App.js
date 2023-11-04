68b1914c1c91f3d896c6a03f4db5ab1607b1f19f
RiwaneAberkane <riwane.aberkane.pro@outlook.fr>
2023-11-04 18:30:25 +0100

    add navbar in app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import React from "react";
+import Navbar from "./components/Navbar";
 import WeatherSearch from "./components/WeatherSearch";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar />
         <WeatherSearch />
       </header>
     </div>
