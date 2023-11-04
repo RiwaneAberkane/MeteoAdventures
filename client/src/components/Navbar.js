47b205a5f93035544fd64b4486fa8870beabd9b0
RiwaneAberkane <riwane.aberkane.pro@outlook.fr>
2023-11-04 18:25:32 +0100

    add navbar.js

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Navbar.css"; // Import du fichier CSS pour les styles
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1>MeteoAdventures</h1>
+      <p>by Riwane</p>
+    </div>
+  );
+};
+
+export default Navbar;
